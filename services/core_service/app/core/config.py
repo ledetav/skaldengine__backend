e8f30f39ef62e477fab27841e6aed86267512312
northern.warmer <nord.hilman@gmail.com>
2026-02-14 08:38:04 +0000

    feat(docker): configure Kafka for event logger and add event logger service

--- services/core_service/app/core/config.py
+++ services/core_service/app/core/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     UPLOAD_DIR: str = "./uploads"
 
     KAFKA_BOOTSTRAP_SERVERS: str = "localhost:9092"
-    KAFKA_TOPIC_EVENTS: str = "skaldengine_entity_events"
+    KAFKA_TOPIC_EVENTS: str = "skaldenginebackend_entity_events"
 
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=True, extra="ignore")
 
