15fcac631ff6aa31c5060cd31fa5da8362ba87df
Данила Сысоев <crocodil-msu@ya.ru>
2026-04-19 19:39:14 +0300

    Update project configs and start script.

--- start.sh
+++ start.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Ожидание и применение миграций для Auth Service
+echo "Applying Auth Service migrations..."
+(
+  cd services/auth_service || exit
+  until alembic upgrade head; do
+    echo "Auth DB is unavailable or starting up - sleeping 2s..."
+    sleep 2
+  done
+  echo "Auth Service migrations applied successfully!"
+)
+
+# Ожидание и применение миграций для Core Service
+echo "Applying Core Service migrations..."
+(
+  cd services/core_service || exit
+  until alembic upgrade head; do
+    echo "Core DB is unavailable or starting up - sleeping 2s..."
+    sleep 2
+  done
+  echo "Core Service migrations applied successfully!"
+)
+
 # Start Nginx in the background
 nginx &
 
